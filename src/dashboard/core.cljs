fb97f632023da4147736c69cc8faa3078652a9b6
RAZAFIMAHEFA Aina <nasoloaina@gmail.com>
2020-04-19 17:47:57 +0200

    Implement app entrypoint in index.html

--- src/dashboard/core.cljs
+++ src/dashboard/core.cljs
@@ -1,5 +1,6 @@
 (ns dashboard.core
-    (:require ))
+    (:require [reagent.core :as r]
+              [reagent.dom :as rdom]))
 
 (enable-console-print!)
 
@@ -9,6 +10,22 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+(defn dashboard-app
+  "The dashboard component"
+  []
+  [:div.container
+   [:h1.title "And we are good to go!!!"]])
+
+(defn start
+    "Mounts the application root component in the DOM."
+    []
+    (rdom/render [dashboard-app] (js/document.getElementById "app")))
+
+(defn ^:export init
+  "Dashboard entrypoint which is called only once when `index.html` loads.
+  It must be exported so it is available even in :advanced release builds."
+  []
+  (start))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
