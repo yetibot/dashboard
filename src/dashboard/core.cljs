f1f44d73c60e4bf02b1d8f859e12bfc610509ffa
RAZAFIMAHEFA Aina <nasoloaina@gmail.com>
2020-04-25 18:09:17 +0200

    Implement left pane menu UI

--- src/dashboard/core.cljs
+++ src/dashboard/core.cljs
@@ -17,27 +17,88 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+(defn nav-bar
+  "Top nav bar"
+  []
+  [:> bloomer/Navbar {:class "is-white is-fixed-top"}
+   [:> bloomer/Container
+    [:> bloomer/NavbarStart
+     [:> bloomer/NavbarBrand
+      [:> bloomer/NavbarItem
+       [:> router/NavLink {:to "/"}
+        [:img {:style {:width  120
+                       :height 28}
+               :class "yetibot-logo"
+               :alt   "Yetibot"
+               :src   "https://yetibot.com/img/yetibot_lambda_blue_with_grey.svg"}]]]]]
+    [:> bloomer/NavbarEnd
+     [:> bloomer/NavbarItem
+      [:> bloomer/Field
+       [search]]]]]])
+
+(defn content-body
+  "Content body"
+  []
+  [:> bloomer/Container {:id "content/body"}
+   [:div {:class "columns"}
+    [:div {:class "column is-2"}
+     [:> bloomer/Menu
+      ;; yetibot
+      [:> bloomer/MenuLabel "Yetibot"]
+      [:> bloomer/MenuList
+       [:li
+        [:> router/NavLink {:exact true
+                             :to    "/"} "Dashboard"]]
+       [:li
+        [:> router/NavLink {:to "/history"} "History"]]
+       [:li
+        [:> router/NavLink {:to "/users"} "Users"]]
+       [:li
+        [:> router/NavLink {:to "/adapters"} "Adapters"]]
+       [:li
+        [:> router/NavLink {:to "/aliases"} "Aliases"]]
+       [:li
+        [:> router/NavLink {:to "/observers"} "Observers"]]
+       [:li
+        [:> router/NavLink {:to "/cron"} "Cron tasks"]]
+       [:li
+        [:> router/NavLink {:to "/repl"} "REPL"]]]
+
+      ;; links
+      [:> bloomer/MenuLabel "Links"]
+      [:> bloomer/MenuList
+       [:li
+        [:a {:href "https://yetibot.com"}
+         [:> bloomer/Icon {:is-size "small"
+                           :is-align "left"
+                           :class "fa fa-external-link-alt"}]
+         "Yetibot.com"]]
+       [:li
+        [:a {:href "https://github.com/yetibot/yetibot"}
+         [:> bloomer/Icon {:is-size "small"
+                           :is-align "left"
+                           :class "fa fa-external-link-alt"}]
+         "Github"]]
+       [:li
+        [:a {:href "https://yetibot.com/archives"}
+         [:> bloomer/Icon {:is-size "small"
+                           :is-align "left"
+                           :class "fa fa-external-link-alt"}]
+         "Blog"]]
+       [:li
+        [:a {:href "https://yetibot.com/user-guide"}
+         [:> bloomer/Icon {:is-size "small"
+                           :is-align "left"
+                           :class "fa fa-external-link-alt"}]
+         "Docs"]]]]]]])
+
 (defn dashboard-app
   "The dashboard component"
   []
-  [:div "testing this"]
   [:> router/BrowserRouter
    [:div
-    [:> bloomer/Navbar {:class "is-white is-fixed-top"}
-     [:> bloomer/Container
-      [:> bloomer/NavbarStart
-       [:> bloomer/NavbarBrand
-        [:> bloomer/NavbarItem
-         [:> router/NavLink {:to "/"}
-          [:img {:style {:width 120
-                         :height 28}
-                 :class "yetibot-logo"
-                 :alt "Yetibot"
-                 :src "https://yetibot.com/img/yetibot_lambda_blue_with_grey.svg"}]]]]]
-      [:> bloomer/NavbarEnd
-       [:> bloomer/NavbarItem
-        [:> bloomer/Field
-         [search]]]]]]]])
+    [nav-bar]
+    [content-body]]])
 
 (defn start
     "Mounts the application root component in the DOM."
