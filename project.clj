1a6b4158218f00954a5b072cbec7ca8f0192b32d
RAZAFIMAHEFA Aina <nasoloaina@gmail.com>
2020-07-14 11:38:55 +0200

    Update dependencies version to avoid conflicts
    Since we are using leiningen to manage dependencies, we have to make sure to
    include/align the versions of the dependencies with those pulled by shadow-cljs,
    especially those involving closure-compiler.
    Instead, if we opt to use shadow-cljs at some point, all the issues related to
    dependencies is solved by the tool and we would not have to deal with those as
    explained here by Thomas Heller : https://clojurians-log.clojureverse.org/shadow-cljs/2020-01-06

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
 
   :min-lein-version "2.9.1"
 
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
 
                  ;; clojurescript and tooling
-                 [org.clojure/clojurescript "1.10.520" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.597" :scope "provided"]
                  [thheller/shadow-cljs "2.8.94"]
-
+                 
                  ;; react
                  [reagent "0.10.0"]
                  [re-frame "0.12.0"]
@@ -30,8 +30,8 @@
                  ;; These has to be explicitly specified as lein does not
                  ;; properly manage dependency version conflicts :
                  ;; https://github.com/thheller/shadow-cljs/issues/488#issuecomment-486732296
-                 [com.google.javascript/closure-compiler-unshaded "v20190325"]
-                 [org.clojure/google-closure-library "0.0-20190213-2033d5d9"]]
+                 [com.google.javascript/closure-compiler-unshaded "v20191027"]
+                 [org.clojure/google-closure-library "0.0-20191016-6ae1f72f"]]
 
   :source-paths ["src"]
 
